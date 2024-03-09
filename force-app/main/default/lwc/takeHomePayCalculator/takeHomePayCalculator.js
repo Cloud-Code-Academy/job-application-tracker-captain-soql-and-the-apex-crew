4c39cb8b63e280c180ed3e6b1a4608778e25a3b0
fritzb100 <71592947+fritzb100@users.noreply.github.com>
2024-03-09 17:00:51 -0500

    got close

--- force-app/main/default/lwc/takeHomePayCalculator/takeHomePayCalculator.js
+++ force-app/main/default/lwc/takeHomePayCalculator/takeHomePayCalculator.js
@@ -7,41 +7,44 @@
 **/
 import { LightningElement } from 'lwc';
 
+// Tax rates
+const federalTaxRate = 0.25;
+const medicareRate = 0.02;
+const socialSecurityRate = 0.03;
+let monthlyTakeHomePay = 0;
+//let salary = 200000;
 export default class TakeHomePayCalculator extends LightningElement {
-
-    salary = 25000;
-    federalTax = 0.175;
-    stateTax = 0.065;
-    medicare = 0.0175;
-    socialSecurity = 0.03;
-    takeHome = '';
-
-
-    get salary() {
-        return this.salary;
-    }
-
-    // set householdIncome(value) {
-    //     this.householdIncome = value;
-    // }
-    handleSalaryChange(event) {
-        this.salary = event.target.value;
-    }
-
-    // handleFirstNameChange(event) {
-    //     this.firstName = event.target.value;
-    // }
-
-    // handleLastNameChange(event) {
-    //     this.lastName = event.target.value;
-    // }
-
-    // handleAgeChange(event) {
-    //     this.age = event.target.value;
-    // }
-
-    // handleHouseholdIncomeChange(event) {
-    //     this.householdIncome = event.target.value;
-    // }
     
+
+    salary = 100000; // Default salary
+
+    // Calculate the take home pay
+    calculateTakeHomePay() {
+        let federalTax = this.salary * federalTaxRate;
+        let medicare = this.salary * medicareRate;
+        let socialSecurity = this.salary * socialSecurityRate;
+        console.log(federalTax);
+        console.log(medicare);
+        console.log(socialSecurity);
+        let totalTax = federalTax + medicare + socialSecurity;
+        console.log(totalTax);
+        let takeHomePay = this.salary - totalTax;
+        console.log(takeHomePay);
+
+        // Convert annual take home pay to monthly
+        this.monthlyTakeHomePay = takeHomePay / 12;
+        console.log(this.monthlyTakeHomePay);
+
+        return this.monthlyTakeHomePay;
+    }
+
+    // Change handler for salary input
+    handleSalaryChange(event) {
+        this.salary = Number(event.target.value);
+        const inputName = event.target.name;
+        if(inputName == 'salaryName') {
+            this.calculateTakeHomePay();
+        }
+        console.log(this.salary);
+    }
 }
