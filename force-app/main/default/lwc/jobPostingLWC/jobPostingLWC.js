d9bb7715e4dd7ffa35da3462ff4af7c94d701aad
Tyler <off.nadir.insight@gmail.com>
2024-03-17 17:09:31 -0700

    displays info on lightning datatable

--- force-app/main/default/lwc/jobPostingLWC/jobPostingLWC.js
+++ force-app/main/default/lwc/jobPostingLWC/jobPostingLWC.js
@@ -5,13 +5,44 @@ import COMPANY_NAME from '@salesforce/label/c.COMPANY_NAME';
 import UPDATED from '@salesforce/label/c.UPDATED';
 import getCalloutData from '@salesforce/apex/jobPostCallout.getCalloutData';
 
+// export default class JobPostingLWC extends LightningElement {
+//     ready=false;
+//     label={
+//         JOBPOSTINGLWC_PAGE_HEADER,
+//         JOB_POST_ID,
+//         COMPANY_NAME,
+//         UPDATED
+//     };
+//     @wire (getCalloutData) callout;
+// }
+
+const columns = [
+    { label: 'Jooble Id', fieldName: 'id' },
+    { label: 'Company Name', fieldName: 'company' },
+    { label: 'Last updated', fieldName: 'updated' },
+];
+
 export default class JobPostingLWC extends LightningElement {
-    ready=false;
-    label={
-        JOBPOSTINGLWC_PAGE_HEADER,
-        JOB_POST_ID,
-        COMPANY_NAME,
-        UPDATED
-    };
-    @wire (getCalloutData) callout;
+    data = [];
+    columns = columns;
+
+    connectedCallback() {
+        const data = [{
+            company: 'Prospect Infosystem Inc',
+            updated: '2024-02-28T00:00:00.0000000',
+            id: '8542290820435711574'
+            },
+            {
+            company: "Apex Informatics",
+            updated: "2024-03-01T00:00:00.0000000",
+            id: 7826091234674837853
+            },
+            {
+            company: "Procare Solutions",
+            updated: "2024-02-28T00:00:00.0000000",
+            id: -8481210526286724053
+        }];
+        this.data = data;
+    }
 }
+
