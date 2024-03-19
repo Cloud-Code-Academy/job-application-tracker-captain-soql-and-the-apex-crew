04807e13ae8d555d389ac4c86cb61e7effb8652c
Tyler <off.nadir.insight@gmail.com>
2024-03-19 16:46:55 -0700

    adds selection actions and button

--- force-app/main/default/lwc/jobPostingLWC/jobPostingLWC.js
+++ force-app/main/default/lwc/jobPostingLWC/jobPostingLWC.js
@@ -1,15 +1,26 @@
 import { LightningElement, wire } from 'lwc';
 import getJsonData from '@salesforce/apex/jobPostCallout.getJsonData';
 
+const ACTIONS = [{label: 'Convert to application'}];
 const columns = [
     { label: 'Jooble Id', fieldName: 'id' },
     { label: 'Company Name', fieldName: 'company' },
     { label: 'Last updated', fieldName: 'updated' },
+    { fieldName: "actions", type: 'action', typeAttributes: {rowActions: ACTIONS}}
 ];
 
 export default class JobPostingLWC extends LightningElement {
     data = [];
     columns = columns;
+    selectedContacts;
+    baseData;
+
+    get selectedJobPostingsLength() {
+        if(this.selectedContacts == undefined) {
+            return 0;
+        }
+        return this.selectedContacts.length;
+    }
 
     @wire(getJsonData) 
     wiredJsonData({ error, data }){
@@ -19,9 +30,26 @@ export default class JobPostingLWC extends LightningElement {
             if (jsonDataObj && jsonDataObj.jobs) {
                 this.data = jsonDataObj.jobs;
             }
+            this.baseData = this.data;
         } else if (error) {
             alert('error: ' + error);
         }
+    }
+
+    handleRowSelection(event) {
+        this.selectedContacts = event.detail.selectedRows;
+    }
+
+    async handleSearch(event) {
+        if(event.target.value == "") {
+            this.contacts = this.baseData;
+        } else if(event.target.value.length > 1) {
+            const searchJobPostings = await searchJobPostings({})
+        }
+    }
+
+    convertPostingToApplication(event) {
+        alert('This is where you could create applications from selected postings some day!');
     }
 }
 
