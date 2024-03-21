93716622964195122b65921129c82a7d0699bbdf
Tyler <off.nadir.insight@gmail.com>
2024-03-21 08:48:35 -0700

    updates to handle new data source

--- force-app/main/default/lwc/jobPostingLWC/jobPostingLWC.js
+++ force-app/main/default/lwc/jobPostingLWC/jobPostingLWC.js
@@ -1,43 +1,56 @@
 import { LightningElement, wire } from 'lwc';
-import getJsonData from '@salesforce/apex/jobPostCallout.getJsonData';
+// import getJsonData from '@salesforce/apex/jobPostCallout.getJsonData';
+import getJobPostings from '@salesforce/apex/jobPostCallout.getJobPostings';
 
-const ACTIONS = [{label: 'Convert to application'}];
+const ACTIONS = [{label: 'Convert to application', name: 'convert'}];
+
 const columns = [
-    { label: 'Jooble Id', fieldName: 'id' },
+    { label: 'Name', fieldName: 'link', type: 'url', typeAttributes: {label: {fieldName: 'jobPostName'}}},
+    { label: 'Jooble Id', fieldName: 'joobleLink', type: 'url', typeAttributes: {label: {fieldName: 'joobleId'}} },
     { label: 'Company Name', fieldName: 'company' },
     { label: 'Last updated', fieldName: 'updated' },
-    { fieldName: "actions", type: 'action', typeAttributes: {rowActions: ACTIONS}}
+    { fieldName: "actions", type: 'action', typeAttributes: {rowActions: ACTIONS}},
 ];
 
 export default class JobPostingLWC extends LightningElement {
-    data = [];
+    // data = [];
     columns = columns;
-    selectedContacts;
-    baseData;
+    selectedPostings;
+    // baseData;
+    jobPostings;
+    wiredJobPostings;
 
     get selectedJobPostingsLength() {
-        if(this.selectedContacts == undefined) {
+        if(this.selectedPostings == undefined) {
             return 0;
         }
-        return this.selectedContacts.length;
+        return this.selectedPostings.length;
     }
 
-    @wire(getJsonData) 
-    wiredJsonData({ error, data }){
-        if (data) {
-            let jsonDataObj = JSON.parse(data);
-
-            if (jsonDataObj && jsonDataObj.jobs) {
-                this.data = jsonDataObj.jobs;
-            }
-            this.baseData = this.data;
-        } else if (error) {
-            alert('error: ' + error);
+    @wire(getJobPostings)
+    jobPostingsWire(result) {
+        this.wiredJobPostings = result;
+        if (result.data) {
+            this.jobPostings = result.data.map((row) => {
+                return this.mapJobPostings(row);
+            });
+        }
+        if (result.error) {
+            console.error(result.error);
         }
     }
 
+    mapJobPostings(row) {
+        return {...row,
+            jobPostName: row.Name,
+            link: `/${row.Id}`,
+            company: row.CompanyRef__c,
+            joobleId: row.JooblePostingId__c,
+            joobleLink: row.Source__c,
+        };
+    }
     handleRowSelection(event) {
-        this.selectedContacts = event.detail.selectedRows;
+        this.selectedPostings = event.detail.selectedRows;
     }
 
     async handleSearch(event) {
